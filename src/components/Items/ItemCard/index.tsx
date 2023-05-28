e14b2cbe5448769de90c073f95531dfb7dbeeeff
Raverside <r4verside@gmail.com>
2023-05-28 00:29:45 -0400

    Initial commit

--- src/components/Items/ItemCard/index.tsx
+++ src/components/Items/ItemCard/index.tsx
@@ -16,13 +16,13 @@ type ItemProps = {
 
 const ItemCard = ({ item }: ItemProps) => {
   return <Card>
-    <img
-      // src={`https://cdn-d.rvezy.com/450x300x80${item.Photos[0]?.Path}`} -- the path doesn't work for some reason (403) and I don't have the time to figure out why
-      src={`https://picsum.photos/200/300`}
-      alt={item.RVName}
-      title={item.RVName}
-      loading="lazy"
-    />
+    {/*the path doesn't work for some reason (403) and I don't have the time to figure out why*/}
+    {/*<img*/}
+    {/*  src={`https://cdn-d.rvezy.com/450x300x80${item.Photos[0]?.Path}`}*/}
+    {/*  alt={item.RVName}*/}
+    {/*  title={item.RVName}*/}
+    {/*  loading="lazy"*/}
+    {/*/>*/}
     <Box>{item.RVName}</Box>
     <Box>{item.DefaultPrice}</Box>
   </Card>;
